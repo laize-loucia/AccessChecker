31f9da4a9a732ae4fc10018e7855d43b572c1ccc
loucia_laize <loucia.laize95@gmail.com>
2026-03-20 23:58:12 +0100

    ajout de commentaires serveur

--- auth-service/app.py
+++ auth-service/app.py
@@ -1,6 +1,17 @@
 from flask import Flask
 import ldap3
 
+
+
+#Ça crée le serveur web
+
+#décorateur.
+#permet de modifier ou enrichir une fonction sans changer son code
+
+#"Quand quelqu’un va sur /health → exécute la fonction health()"
+
+#app = serveur flask
+
 app = Flask(__name__)
 
 @app.route("/health")
